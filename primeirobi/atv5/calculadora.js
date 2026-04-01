a991423fcb80b2e3aaf0c8b3489cc86b634c93b0
Gabriel Morais <gabrielmorais290921@gmail.com>
2026-03-31 21:18:44 -0300

    Atividade 5, first JS

---
+++ primeirobi/atv5/calculadora.js
@@ -0,0 +1,6 @@
+const valorConta = 100
+const percentualGorjeta = 15
+const valorGorjeta = valorConta * (percentualGorjeta/100)
+const valorTotal = valorConta + valorGorjeta
+
+console.log("Valor da Conta:", valorConta,", Gorjeta (",percentualGorjeta,"%):", valorGorjeta, "Total a pagar:", valorTotal)
